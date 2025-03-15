29b2d4a2dc44db4df65930c8e5a3e337db9d4437
Laksito Pamilih <laksito@protonmail.com>
2025-03-15 17:44:42 +0700

    install tailwind

--- src/components/ReactCounter.tsx
+++ src/components/ReactCounter.tsx
@@ -4,7 +4,10 @@ export default function ReactCounter (): React.JSX.Element {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setCount(n => ++n)}>
+      <button
+        className="bg-amber-700 py-4 px-8 rounded-2xl cursor-pointer"
+        onClick={() => setCount(n => ++n)}
+      >
         {count}
       </button>
     </div>
